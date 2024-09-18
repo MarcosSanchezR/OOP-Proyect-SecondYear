8037884e9543180054cf9622d8b77b15a0454853
marcossanchez <m.srobles@alumnos.upm.es>
2024-09-18 18:47:59 +0200

    atributos añadidos

--- src/main/java/upm/User.java
+++ src/main/java/upm/User.java
@@ -1,6 +1,9 @@
 package upm;
 
 public class User {
+    private static final int EDAD_MAXIMA=30;
+    private static final int EDAD_MINIMA=18;
     private String nombre;
+    private int edad;
 
 }
