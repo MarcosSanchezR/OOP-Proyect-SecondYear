96495a24eb9f9c246e9e7cefc9d84109fe273c62
marcossanchez <m.srobles@alumnos.upm.es>
2024-09-24 09:27:42 +0200

    codigo actualizado

--- src/main/java/upm/User.java
+++ src/main/java/upm/User.java
@@ -1,20 +1,25 @@
 package upm;
 
+import java.time.LocalDate;
+
+
 public class User {
     private static final int EDAD_MAXIMA = 30;
     private static final int EDAD_MINIMA = 18;
     private String nombre;
+    private LocalDate fechaNacimineto;
     private int edad;
     private String dni;
     private String email;
     private String genero;
 
-    public User(String nombre, int edad, String dni, String email, String genero) {
+    public User(String nombre, LocalDate fechaNacimineto, String dni, String email, String genero) {
         this.nombre = nombre;
-        this.edad = edad;
+        this.fechaNacimineto = fechaNacimineto;
         this.dni = dni;
         this.email = email;
-        this.genero= genero;
+        this.genero = genero;
+        this.edad= LocalDate.now().getYear()- fechaNacimineto.getYear();
     }
 
     public boolean esDni(String dni) {
@@ -27,23 +32,13 @@ public class User {
         return digitos && letra;
     }
 
-    public boolean esEmail(String email){
-        boolean resul = false;
-        if (email.contains("@")){
-            String[] partes = email.split("@");
-            if (partes.length == 2 && partes[1].contains(".")){
-                String[] dominio= partes[1].split("\\.");
-                if (dominio.length == 2){
-                    resul=true;
-                }
-            }
-
-        }
-        return resul;
+    public void cambiarNombre(String nuevo){
+        setNombre(nuevo);
     }
 
-
-
+    public void cambiarEmail(String nuevo){
+        setEmail(nuevo);
+    }
 
     public String getNombre() {
         return nombre;
@@ -83,7 +78,7 @@ public class User {
     }
 
     public void setEmail(String email) {
-        if (esEmail(email)){
+        if (email.endsWith("@gmail.com") || email.endsWith("@hotmail.com")){
             throw new IllegalArgumentException("El mail no es valido");
         }
         this.email = email;
@@ -98,6 +93,14 @@ public class User {
             throw new IllegalArgumentException("El genero no es valido");
         }
         this.genero = genero;
+    }
+
+    public LocalDate getFechaNacimineto() {
+        return fechaNacimineto;
+    }
+
+    public void setFechaNacimineto(LocalDate fechaNacimineto) {
+        this.fechaNacimineto = fechaNacimineto;
     }
 }
 
